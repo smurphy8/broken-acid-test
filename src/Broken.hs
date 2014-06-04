c9c42e6b23bcbeb79c965008ca9be98e07bb8256
Scott Murphy <scottmurphy09@gmail.com>
2014-06-04 15:08:14 -0500

    Bug source looks to be in System.IO

--- src/Broken.hs
+++ src/Broken.hs
@@ -10,19 +10,38 @@ import Data.Acid            ( AcidState, Query, Update
 
 
 import Data.Traversable
-
+import qualified System.IO as SIO
 import Control.Applicative  ( (<$>) )
 import Control.Exception    ( bracket )
 import Control.Monad        ( msum )
 import Control.Monad.Reader ( ask )
 import Control.Monad.State  ( get, put )
 import Control.Concurrent
+import System.Posix.Process (getProcessID)
 import Data.Typeable
 import Data.Acid.Advanced   ( query', update' )
 import Data.Acid.Local      ( createCheckpointAndClose )
 import Data.SafeCopy        ( base, deriveSafeCopy )
-import Filesystem
-import Filesystem.Path
+import Filesystem hiding (writeFile,openFile)
+import Filesystem.Path hiding (FilePath)
+
+
+import System.Posix(Fd(Fd),
+                    openFd,
+                    fdWriteBuf,
+                    fdToHandle,
+                    closeFd,
+                    OpenMode(WriteOnly,ReadWrite),
+                    exclusive, trunc,
+                    defaultFileFlags,
+                    stdFileMode
+                   )
+
+open :: FilePath -> IO Handle
+open filename = ( openFd filename ReadWrite (Just 0o600) (defaultFileFlags {exclusive = True, trunc = True})) >>= fdToHandle
+
+data FHandle = FHandle Fd
+
 
 data CounterState = CounterState { count :: Integer }
     deriving (Eq, Ord, Read, Show,  Typeable)
@@ -55,5 +74,35 @@ openABunchOfStates n = do
   setWorkingDirectory wd
   where
     stateOpenFcn fn = do
-      threadDelay (1000)
+      threadDelay (10*1000)
       openLocalStateFrom fn initialCounterState
+
+
+writeABunchOfFiles n = do
+  let aBunchOfFiles = fmap show [1 .. n]
+  wd <- getWorkingDirectory 
+  setWorkingDirectory (wd </> "stressIO")
+  traverse writeFileFcn aBunchOfFiles
+  setWorkingDirectory wd
+  where
+    writeFileFcn fn = do
+      writeFile fn "Is this a bug?"
+
+
+openABunchOfFiles n = do
+  let aBunchOfFiles = fmap show [1 .. n]
+  wd <- getWorkingDirectory 
+  setWorkingDirectory (wd </> "stressIO")
+  hs <- traverse openFileFcn aBunchOfFiles
+  setWorkingDirectory wd
+  return hs
+  where
+    openFileFcn fn = do
+      open fn 
+
+openAndCloseABunchOfFiles n = do
+  hs <-(openABunchOfFiles n)  
+  traverse (\h -> do
+--               pid <- getProcessID
+               SIO.hPutStrLn h (show "am I a bug?")
+               SIO.hClose h ) hs
--- src/Main.hs
+++ src/Main.hs
@@ -8,10 +8,14 @@ import Data.Acid            ( AcidState, Query, Update
 import Data.Acid.Advanced   ( query', update' )
 import Data.Acid.Local      ( createCheckpointAndClose )
 import Data.SafeCopy        ( base, deriveSafeCopy )
-
+import Control.Exception
 import Broken
 
 main = do
   print "stressing system"
-  openABunchOfStates 2000
+--  openABunchOfStates 2000
+--  writeABunchOfFiles 2000
+--  openABunchOfFiles 2000
+  openAndCloseABunchOfFiles 2000
+  
        
